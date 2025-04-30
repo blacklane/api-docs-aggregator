01df457c6c987fac8d17719685f4659e8bf916ef
vinmarco <109508503+vinmarco@users.noreply.github.com>
2025-04-30 12:06:59 +0200

    fix typo

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda_exec" {
-  name = "GithubProxyLeambda"
+  name = "GithubProxyLambda"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
