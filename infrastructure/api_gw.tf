cc5622281763206313d725997aee4f4d5b532968
vinmarco <109508503+vinmarco@users.noreply.github.com>
2025-04-30 12:21:50 +0200

    add depends on

--- infrastructure/api_gw.tf
+++ infrastructure/api_gw.tf
@@ -44,6 +44,10 @@ resource "aws_api_gateway_integration" "lambda-integration-mock" {
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda-integration,
+    aws_api_gateway_integration.lambda-integration-mock
+  ]
   rest_api_id = aws_api_gateway_rest_api.github-proxy-api.id
   lifecycle {
     create_before_destroy = true
