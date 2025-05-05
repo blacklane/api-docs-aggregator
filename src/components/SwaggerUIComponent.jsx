1242b9860809bfb41c2d27a3088fc7be9f38ac3a
vinmarco <109508503+vinmarco@users.noreply.github.com>
2025-05-05 12:30:19 +0200

    revert previous commit

--- src/components/SwaggerUIComponent.jsx
+++ src/components/SwaggerUIComponent.jsx
@@ -16,11 +16,10 @@ function SwaggerUIComponent({ url, apiBaseUrl }) {
 				req,
 			);
 		}
-		req;
-		// return new Request(
-		// 	`${apiBaseUrl}?url=${encodeURIComponent(req.url)}`,
-		// 	req,
-		// );
+		return new Request(
+			`${apiBaseUrl}?url=${encodeURIComponent(req.url)}`,
+			req,
+		);
 	};
 
 	return (
