5efbd0c506ff0c464e48b8225a1773847365f740
vinmarco <109508503+vinmarco@users.noreply.github.com>
2025-05-14 17:32:51 +0200

    fix data provider

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,6 +1,5 @@
 data "aws_route53_zone" "sdlc-blacklane-io" {
-  provider = aws.us-east-1
-  name     = "sdlc.blacklane.io."
+  name = "sdlc.blacklane.io."
 }
 
 data "aws_partition" "current" {}
@@ -10,5 +9,6 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 data "aws_acm_certificate" "cert" {
-  domain = "*.sdlc.blacklane.io"
+  provider = aws.us-east-1
+  domain   = "*.sdlc.blacklane.io"
 }
