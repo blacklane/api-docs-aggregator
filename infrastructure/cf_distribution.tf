d1a20fffbe0bfede1578f3d97a5754d501f92cc7
vinmarco <109508503+vinmarco@users.noreply.github.com>
2025-05-15 14:11:03 +0200

    changed s3 origin

--- infrastructure/cf_distribution.tf
+++ infrastructure/cf_distribution.tf
@@ -28,23 +28,32 @@ resource "aws_cloudfront_distribution" "frontend" {
     domain_name = aws_s3_bucket.api-doc.bucket_regional_domain_name
     origin_id   = "api-docs"
 
-    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only" # S3 website endpoints only support HTTP
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
   }
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "api-docs"
-
+    target_origin_id       = "api-docs"
     viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods  = ["GET", "HEAD"]
 
     forwarded_values {
       query_string = false
+
       cookies {
         forward = "none"
       }
     }
 
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
   }
 
   viewer_certificate {
@@ -52,6 +61,8 @@ resource "aws_cloudfront_distribution" "frontend" {
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
+
+  price_class = "PriceClass_All"
 
   restrictions {
     geo_restriction {
